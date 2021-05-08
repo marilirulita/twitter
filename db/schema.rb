eb1592eb2c72996c87006e406845bd718efd256f
marysol <marilirulita@gmail.com>
2021-05-08 07:36:02 -0700

    Add route controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_111913) do
+ActiveRecord::Schema.define(version: 2021_05_08_131953) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
